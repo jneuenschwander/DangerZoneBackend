b7acd965cbe107b1b11fe07a24bd425f9a7bf6d5
Juan Antonio <41336919+jneuenschwander@users.noreply.github.com>
2020-05-02 23:18:23 -0500

    se soluciono el error
    http://localhost:3000/crimes/ esta es la ruta XD

--- src/controllers/crime.js
+++ src/controllers/crime.js
@@ -3,18 +3,19 @@ const Crime = require('../models/crime');
 
 module.exports = {
     index: async(req, res, next) => {
+        //res.json("Holaaaa")
         try{
             const crimes = await Crime.find({},function(err,collection){console.log(collection)}).limit(20);
             if(crimes){
                 res.status(200).json(crimes);
             }else{
-                //res.status(404).json({error : "no fue encontrado ningun dato"})
+                res.status(404).json({error : "no fue encontrado ningun dato"})
                 console.log("error!!!!!!!");
             }
         }catch(err){
             res.status(500).json({error : "Ocurrio un error db:"+ err})
         }
-
+  
     },
 
 
