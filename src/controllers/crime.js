d52ac494ca35bd2e511b009ff12ad53222700fcc
Juan Antonio <41336919+jneuenschwander@users.noreply.github.com>
2020-05-04 17:50:34 -0500

    peticion de google map

--- src/controllers/crime.js
+++ src/controllers/crime.js
@@ -2,8 +2,8 @@ const Crime = require('../models/crime');
 
 
 module.exports = {
+    //localhost:3000/crimes/
     index: async(req, res, next) => {
-        //res.json("Holaaaa")
         try{
             const crimes = await Crime.find({},function(err,collection){console.log(collection)}).limit(20);
             if(crimes){
@@ -15,8 +15,16 @@ module.exports = {
         }catch(err){
             res.status(500).json({error : "Ocurrio un error db:"+ err})
         }
-  
     },
+    googleMap: async(req,res, next) =>{
+        try{
+            const crimes = await Crime.find({latitud:1,longitud:1}).where({Location:{$ne:""},Year:{$eq:"2018"}}).limit(100); // falta hacer que solo salgan los campos de latitud y longitud
+            
+            res.json(crimes);
+        }catch(err){
+            res.status(500).json({error : "Ocurrio un error db:"+ err});
+        }
+    }
 
 
 
--- src/routes/crime.js
+++ src/routes/crime.js
@@ -2,11 +2,11 @@ const router = require('express-promise-router')();
 
 const {
     //aqui van los controladores
-    index
+    index, googleMap
 } =require('../controllers/crime')
 
 router.get('/',index);
-
+router.get('/googleMap',googleMap)
 //router.post('/',newUser);
 //router.get('/:userId', getUser);
 /*
